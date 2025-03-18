d399f06ed14066eecbc1c86bac6eba14e3bf3105
Olli Nevalainen <olli.nevalainen.fmi@gmail.com>
2025-03-18 15:18:36 +0200

    updated __init__

--- smhi_open_data/__init__.py
+++ smhi_open_data/__init__.py
@@ -1,8 +1,10 @@
 from smhi_open_data.client import SMHIOpenDataClient
 from smhi_open_data.enums import Parameter
+from smhi_open_data.utils import combine_archived_and_latest_months
 
 
 __all__ = [
     "SMHIOpenDataClient",
     "Parameter",
+    "combine_archived_and_latest_months",
 ]
